296dd708d3984fae23164ed3af96119a18b1cb8b
Tom Spencer <thomas.spencer2034@gmail.com>
2023-09-18 11:24:24 -0400

    created client.js file and added connect function to it. Exported connect function from client.js and imported it to play.js

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-});
-
-conn.setEncoding("utf8");
-
-conn.on("data", (data) => {
-  console.log("Server says: ", data);
-});
-conn.on("close", () => {
-  conn.log("you ded cuz you idled");
-});
-
-return conn;
-
-};
+const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
