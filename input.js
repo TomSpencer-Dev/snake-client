5e7002c5f5deacff3a133faae881eb163c92dd02
Tom Spencer <thomas.spencer2034@gmail.com>
2023-09-19 09:56:28 -0400

    Updated play.js to send object of connect() to setupInput(). Assigned return value from connect() to connection variable and used to pass 'move: [direction]' to server based on keystrokes w,a,s,d, using a switch statement

--- input.js
+++ input.js
@@ -1,20 +1,38 @@
+let connection;
+
 // "data" callback handler for stdin.
 
-const handleUserInput = function() {
-  if (data === '\u0003') {
-    process.exit;
-  }
+const handleUserInput = function(data) {
 
+  switch (data) {
+    case '\u0003':
+      process.exit();
+    case "w":
+      connection.write("Move: up");
+      break;
+    case "a":
+      connection.write("Move: left");
+      break;
+    case "s":
+      connection.write("Move: down");
+      break;
+    case "d":
+      connection.write("Move: right");
+      break;
+  }
 };
 
 // setup interface to handle user input from stdin
 
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput(data));
+  stdin.on("data", data => {
+    handleUserInput(data);
+  });
   return stdin;
 };
 
